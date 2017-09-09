c72b09ad4e3d94d9bcfb72a67db9d2f0e884d5c5
Kavani86 <31796299+Kavani86@users.noreply.github.com>
2017-09-09 20:31:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+     class Firstlesson
+{
+    static void Main()
+    {
+        Console.WriteLine("fuck you");  //здесь тебя посылают нахуй
+        int Fuck;
+        for (Fuck = 1; Fuck < 10; Fuck++)
+        Console.WriteLine("Fuck you all  {0,2} times",Fuck);
+        Console.ReadKey();
+    }
+}
+  
